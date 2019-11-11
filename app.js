721c3e7766f81ba05be46ad65d60b4d77f5391bb
georg.luik <georg.luik@khk.ee>
2019-11-11 11:43:15 +0200

    väli/sisemõõdud - scroll - redirect - reload

--- app.js
+++ app.js
@@ -4,13 +4,48 @@
 // window.alert('Tere hommikust');
 
 // prompt
-const sisend =prompt('Sisesta oma nimi');
-alert('Tere Hommikust, ' + sisend +
-'!');
+// const sisend =prompt('Sisesta oma nimi');
+// alert('Tere Hommikust, ' + sisend +
+// '!');
 
 // confirm
-if(confirm('Kas oled nõus?')){
+/* if(confirm('Kas oled nõus?')){
     console.log('jah');  
 } else {
     console.log('ei');
-}
+} */
+
+
+// välimõõdud
+let valiKorgus = window.outerHeight;
+let valiLaius = window.outerWidth;
+
+console.log(valiKorgus, valiLaius);
+
+// sisemõõdud
+let siseKorgus = window.innerHeight;
+let siseLaius = window.innerWidth;
+
+// console.log(siseLaius);
+
+/* scroll
+scrollY = window.scrollY;
+scrollX = window.scrollX;
+
+console.log(scrollY);  */
+
+// location objekt
+let objekt = window.location;
+let hostname = window.location.hostname;
+let port = window.location.port;
+let href = window.location.href;
+let search = window.location.search;
+console.log(hostname);
+
+// redirect 
+window.location.href = 'https://khk.ee';
+
+// reload
+window.location.reload();
+
+
