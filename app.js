e56ffe32249ff1619fd550f0fd4c98b440b5ce0c
georg.luik <georg.luik@khk.ee>
2019-11-12 15:34:08 +0200

    dokumenti stiili muutmine

--- app.js
+++ app.js
@@ -1,43 +1,15 @@
-// document
+// document meetodid
+// single element selectors
 
-let sisu;
+sisu = document.getElementById('add-task'); // html sisu
+id = document.getElementById('add-task').id; // id 
+klass = document.getElementById('add-task').className; 
 
-sisu = document; // sisu html-is
-sisu = document.all; // html tagis kasutamisjärjekorras
+// stiili muutmine 
 
-sisu = document.all[2]; // konkreetne html tag
-sisu = document.all.length; // palju on html tagi kasutusel
-
-// document properties
-sisu = document.head; // head sisu
-sisu = document.body; // body sisu
-sisu = document.doctype; // doctype
-sisu = document.domain; // 
-sisu = document.URL;
-sisu = document.characterSet;
-sisu = document.contentType; 
-
-// formi andmed
-sisu = document.forms; // kõik formid
-sisu = document.forms[0]; // esimene form
-sisu = document.forms[0].id; // esimese formi id
-sisu = document.forms[0].method; // formi töötlus meetod
-sisu = document.forms[0].action;
-
-// linkide andmed
-sisu = document.links; // kõik lingid
-sisu = document.links[0]; // esimene link
-sisu = document.links[0].className; // lingi klassid
-sisu = document.links[0].classList[1]; // klasside massiiv ja sealt elemendi lugemine 
-
-// pildid
-sisu = document.images;
-
-// skriptid
-sisu = document.scripts;
-sisu = document.scripts[0].getAttribute('src'); // materialize js
-sisu = document.scripts[1].getAttribute('src'); // local js
-
+sisu.style.background = '#333';
+sisu.style.color = '#fff';
+sisu.style.padding = '5px';
 
 console.log(sisu);
 
