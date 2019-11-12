473944228d36261e5257742c9908411f3880017e
georg.luik <georg.luik@khk.ee>
2019-11-12 15:37:11 +0200

    sisu muutmine

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ sisu.style.background = '#333';
 sisu.style.color = '#fff';
 sisu.style.padding = '5px';
 
+
+// sisu muutmine
+sisu.textContent = 'Minu Ülesanded';
+sisu.innerText = 'Just minu ülesanded';
+sisu.innerHTML = '<span style="color:green">Vabad ülesanded</span>';
 console.log(sisu);
 
 
