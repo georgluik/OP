caae3518a699c36ba7bb5015c3a8f726712a7610
georg.luik <georg.luik@khk.ee>
2019-11-12 15:22:06 +0200

    lingid - pildid - skriptid

--- app.js
+++ app.js
@@ -24,6 +24,21 @@ sisu = document.forms[0].id; // esimese formi id
 sisu = document.forms[0].method; // formi töötlus meetod
 sisu = document.forms[0].action;
 
+// linkide andmed
+sisu = document.links; // kõik lingid
+sisu = document.links[0]; // esimene link
+sisu = document.links[0].className; // lingi klassid
+sisu = document.links[0].classList[1]; // klasside massiiv ja sealt elemendi lugemine 
+
+// pildid
+sisu = document.images;
+
+// skriptid
+sisu = document.scripts;
+sisu = document.scripts[0].getAttribute('src'); // materialize js
+sisu = document.scripts[1].getAttribute('src'); // local js
+
+
 console.log(sisu);
 
 
