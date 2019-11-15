0be99b4bfc16bccb6e4610878b740b558e3466a7
georg.luik <georg.luik@khk.ee>
2019-11-15 13:45:38 +0200

    node omadused ja meetodid

--- app.js
+++ app.js
@@ -1,41 +1,50 @@
 // document meetodid
-// multiply elements selectors
+// node omadused ja meetodid
 
-// klassi nime järgi
-let sisu = document.getElementsByClassName('collection-item');
+let nimekiri = document.querySelectorAll('ul.collection');
 
+let nimekirjaElement = document.querySelectorAll('li.collection-item');
+console.log(nimekiri);
+console.log(nimekirjaElement);
+
+nimekiri = document.querySelector('ul.collection');
+console.log(nimekiri);
+
+nimekirjaElement = nimekiri.childNodes;
+console.log(nimekirjaElement);
+
+sisu = nimekiri.childNodes;
+sisu = nimekiri.childNodes[0];
+sisu = nimekiri.childNodes[0].nodeName;
+sisu = nimekiri.childNodes[3].nodeType;
+
+/*
+erinevad node tüübid
+1 - element
+2 - atribuut
+3 - tekst
+8 - kommentaar
+9 - dokument
+10 - doctype
+*/
+
+sisu = nimekiri.children;
+sisu = nimekiri.children[1];
+nimekiri.children[1].textContent = 'Õpi rohkem JS';
+nimekiri.children[2].children[0].id = 'katse';
 console.log(sisu);
-console.log(sisu[0]);
-sisu[0].style.color = 'orange';
-sisu[2].textContent = 'Õpi Arvutivõrgud';
 
-// querySelector + klassi nime järgi
-let nimekirjaElemendid = document.querySelector('ul').getElementsByClassName('collection-item');
+sisu = nimekiri.firstChild; // node type
+sisu = nimekiri.firstElementChild; // value
 
-// elemendi nime järgi
-nimekirjaElemendid = document.getElementsByTagName('li');
+sisu = nimekiri.childElementCount;
 
-// teisendame HTMLCollection massiiviks (Array)
-nimekirjaElemendid = Array.from(nimekirjaElemendid);
-// massiivi meetodite kasutamine
-nimekirjaElemendid.reverse();
+nimekirjaElement = document.querySelector('li.collection-item');
+sisu = nimekirjaElement.parentNode;
+sisu = nimekirjaElement.parentElement;
+sisu = nimekirjaElement.parentElement.parentElement;
 
-// massiivi läbimine tsükliga
-// for
-for(let i = 0; i < 3; i++){
-    console.log(nimekirjaElemendid[i].className)
-}
+sisu = nimekirjaElement.nextSibling; // node type
+sisu = nimekirjaElement.nextElementSibling;
+console.log(sisu);
 
-// for each
-// nimekirjaElemendid.forEach(function(element){
-//    console.log(element);
-//    element.style.background = '#ddd';
-// });
-
-let nimekiriPaaritu = document.querySelectorAll('li:nth-child(odd)');
-// console.log(nimekiriPaaritu)
-;
-
-let nimekiriPaaris = document.querySelectorAll('li:nth-child(even)')
-; 
-console.log(nimekiriPaaris)
