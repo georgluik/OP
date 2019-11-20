4e6b83763fe6ea9a655eda5cd7e7b513cbc2b0a6
georg.luik <georg.luik@khk.ee>
2019-11-20 08:54:08 +0200

    event sisene funktsioon (click)

--- app.js
+++ app.js
@@ -1,37 +1,8 @@
-// document meetodid
-// elementide kustutamine
+// sündmused
+// click
 
-// leia antud DOM-is kõik li elemendid
-
-const liElemendid = document.querySelectorAll('li');
-// kustuta konkreetne element
-liElemendid[2].remove();
-
-// leia antud DOM-is ul element
-
-const ulElement = document.querySelector('ul');
-// kustuta elemendist li
-// li on ul child element
-
-ulElement.removeChild(liElemendid[1]);
-
-// nimekirja element
-const liElement = liElemendid[0];
-// kustutamislink
-const link = liElement.children[0];
-// class ja attribute kustutamine
-// class
-let sisu = link.className;
-sisu = link.classList;
-sisu = link.classList[1];
-
-link.classList.remove('secondary-content');
-link.classList.add('secondary-content');
-
-// atribuut
-sisu = link.getAttribute('href');
-link.setAttribute('href', 'https://google.com');
-link.setAttribute('title', 'Google');
-link.removeAttribute('title');
-sisu = link;
-console.log(sisu);
+const kustutaKoik = document.querySelector('.clear-tasks');
+kustutaKoik.addEventListener('click', function(e){
+    console.log('vajutatud');
+   // e.preventDefault();
+});
