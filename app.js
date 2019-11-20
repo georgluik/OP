37b255417d2b91b71a8086164c608cde08585a98
georg.luik <georg.luik@khk.ee>
2019-11-20 10:28:50 +0200

    sündmused vorm

--- app.js
+++ app.js
@@ -1,38 +1,22 @@
 // sündmused
 // click
 
-const kustutaKoik = document.querySelector('.clear-tasks');
+const form = document.querySelector('form');
+const lisaUusYlesanne = document.getElementById('task');
 const text = document.querySelector('h5');
-const kaart = document.querySelector('.card');
-// click
-kustutaKoik.addEventListener('click', syndmus);
 
-// double click
-kustutaKoik.addEventListener('dblclick', syndmus);
+// kustutame lisaUusYlesanne väärtus
+lisaUusYlesanne.value = '';
 
-kustutaKoik.addEventListener('mousedown', syndmus);
-kustutaKoik.addEventListener('mouseup', syndmus);
+// vormi saatmine
+form.addEventListener('submit', syndmus );
 
-// mouse enter
-kaart.addEventListener('mouseenter', syndmus)
-// mouse leave
-kaart.addEventListener('mouseleave', syndmus);
-
-// mouse over
-kaart.addEventListener('mouseover', syndmus);
-
-// mouse out
-kaart.addEventListener('mouseout', syndmus);
-
-// mouse move
-kaart.addEventListener('mousemove', syndmus);
 function syndmus(e){
     // logi sündmuse tüüp
     console.log(`Sündmuse tüüp: ${e.type}`);
-    // muuda text sisu hiirX x koord hiirY y koord
-    text.textContent = 'HiirX: ' + e.offsetX + ' HiirY: ' + e.offsetY;
-    //  text.textContent = `HiirX: ${e.offsetX}`;
-
-    // koosta body värv rgb(Xkoord, Ykoord, 40)
-    document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 40)`
+    // kontrollime vormi kaudu saadetud väärtus
+    console.log(lisaUusYlesanne.value);
+    // väljastame ülesanne HTMLis
+    text.innerText = lisaUusYlesanne.value;
+    
 }
