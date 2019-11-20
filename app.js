752bd852ea697e3607ea319a99f562a0684090ef
georg.luik <georg.luik@khk.ee>
2019-11-20 09:23:58 +0200

    sündmused - event objekti omadused

--- app.js
+++ app.js
@@ -5,5 +5,21 @@ const kustutaKoik = document.querySelector('.clear-tasks');
 kustutaKoik.addEventListener('click', vajutaNupp);
 
 function vajutaNupp(e){
-    console.log('vajutatud');
+    let sisu = e;
+    sisu = e.target;
+    sisu = e.target.id;
+    sisu = e.target.className;
+    sisu = e.target.classList;
+
+    // tüüp
+    sisu = e.type;
+
+    // sündmuse koordinaadid browseri akna suhtes
+    sisu = e.clientY;
+    sisu = e.clientX;
+
+    // sündmuse koordinaadid elemendi enda suhtes
+    sisu = e.offsetY;
+    sisu = e.offsetX;
+    console.log(sisu);
 }
