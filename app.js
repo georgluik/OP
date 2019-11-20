70c036eab2c1d5d17c8e287902025cc620cf7e4b
georg.luik <georg.luik@khk.ee>
2019-11-20 12:16:57 +0200

    local storage andmete salvestamine ja lugemine

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
-// sündmused
+// storage
 
-// lehe kaardi pealkiri
-/*
- document.querySelector('.card-title').addEventListener('click', function(e){
-    console.log('card-title');
-});
+// salvestame väärtus
+// localStorage.setItem('eesnimi', 'Georg');
+// localStorage.setItem('perenimi', 'Luik')
 
-// lehe kaardi sisu
-document.querySelector('.card-content').addEventListener('click', function(e){
-    console.log('card-content');
-}); 
+// väärtuste eemaldamine
 
-*/
+// localStorage.removeItem('perenimi');
 
-// delegeerimine
+// väärtuste lugemine
+// onst eesnimi = localStorage.getItem('eesnimi');
+// console.log(eesnimi);
 
+// eemalda kõik väärtused
+// localStorage.clear();
 
+document.querySelector('form').addEventListener('submit', salvesta);
 
-const kustutaYlesanne = document.querySelector('.fa-remove');
-kustutaYlesanne.addEventListener('click', kustuta);
-
-function kustuta(e){
-    console.log(e.target.parentElement);
-    if(e.target.parentElement.classList.contains('delete-item')){
-        console.log('hakkame kustutama')
-    e.target.parentElement.parentElement.remove();
+function salvesta(e){
+    const uusYlesanne = document.getElementById('task').value;
+    // localStorage.setItem('tasks', uusYlesanne);
+    
+    let ylesanded;
+    if(localStorage.getItem('tasks') === null){
+        ylesanded = [];
+        // console.log(ylesanded);
+    } else {
+        ylesanded = JSON.parse(localStorage.getItem('tasks'));
     }
+    console.log(ylesanded);
+    ylesanded.push(uusYlesanne);
+    console.log('ülesanne lisatud')
+    localStorage.setItem('tasks', JSON.stringify(ylesanded));
+
+    e.preventDefault();
 }
 
+// loeme sisestatud ülesanded
+const ylesanded = JSON.parse(localStorage.getItem('tasks'));
+ylesanded.forEach(ylesanne => {
+    console.log(ylesanne);
+});
