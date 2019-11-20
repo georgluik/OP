57c947d1a546d3d0b6d30e2a3a8a15bd711c3744
georg.luik <georg.luik@khk.ee>
2019-11-20 11:27:06 +0200

    sündmuste elementidest sisse ja välja suunamine

--- app.js
+++ app.js
@@ -1,45 +1,30 @@
 // sündmused
-// click
 
-const form = document.querySelector('form');
-const lisaUusYlesanne = document.getElementById('task');
-const text = document.querySelector('h5');
+// lehe kaardi pealkiri
+/*
+ document.querySelector('.card-title').addEventListener('click', function(e){
+    console.log('card-title');
+});
+
+// lehe kaardi sisu
+document.querySelector('.card-content').addEventListener('click', function(e){
+    console.log('card-content');
+}); 
+
+*/
+
+// delegeerimine
 
 
-// kustutame lisaUusYlesanne väärtus
-lisaUusYlesanne.value = '';
 
-// klaviatuur
-// keydown
-lisaUusYlesanne.addEventListener('keydown', syndmus);
+const kustutaYlesanne = document.querySelector('.fa-remove');
+kustutaYlesanne.addEventListener('click', kustuta);
 
-// keyup
-// lisaUusYlesanne.addEventListener('keyup', syndmus);
-
-// keypress
-lisaUusYlesanne.addEventListener('keypress', syndmus);
-
-// focus
-lisaUusYlesanne.addEventListener('focus', syndmus);
-
-// blur
-lisaUusYlesanne.addEventListener('blur', syndmus);
-
-// copy
-lisaUusYlesanne.addEventListener('cut', syndmus);
-
-// paste
-lisaUusYlesanne.addEventListener('paste', syndmus);
-
-// input
-lisaUusYlesanne.addEventListener('input', syndmus);
-
-function syndmus(e){
-    // logi sündmuse tüüp
-    console.log(`Sündmuse tüüp: ${e.type}`);
-    // kontrollime vormi kaudu saadetud väärtus
-    console.log(lisaUusYlesanne.value);
-    // väljastame ülesanne HTMLis
-    text.innerText = lisaUusYlesanne.value;
-    
+function kustuta(e){
+    console.log(e.target.parentElement);
+    if(e.target.parentElement.classList.contains('delete-item')){
+        console.log('hakkame kustutama')
+    e.target.parentElement.parentElement.remove();
+    }
 }
+
