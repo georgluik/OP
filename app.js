55a047ce1673858b56718253d9ab2ec0d82e2afd
georg.luik <georg.luik@khk.ee>
2019-11-25 15:05:25 +0200

    raamatud väljade puhastamine

--- app.js
+++ app.js
@@ -6,6 +6,20 @@ function Raamat(p, a, i){
     this.isbn = i;
 }
 
+// kasutajaliidese(KL) konstruktor
+function KL(){
+
+}
+
+// KL funktsionaal
+// sisendväljade puhastamine
+KL.prototype.puhastaSisend = function(){
+    document.getElementById('title').value = '';
+    document.getElementById('autor').value = '';
+    document.getElementById('isbn').value = '';
+}
+
+
 // kirjeldame raamatu lisamise sündmust
 document.getElementById('book-form').addEventListener('submit', lisaRaamat);
 // raamatu lisamise funktsioon
@@ -16,6 +30,12 @@ function lisaRaamat(e){
     // loome raamat andmete põhjal
     const raamat = new Raamat(pealkiri, autor, isbn);
     console.log(raamat);
+
+    // loome kasutajaliidese objekt temaga opereerimiseks
+    const kl = new KL();
+    // puhastame väljad sisestatud andmetest
+    kl.puhastaSisend();
+
 
     e.preventDefault();
 }
