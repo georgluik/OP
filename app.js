09d5de6e2e6a9daf5e175337729ef851e122ebe3
georg.luik <georg.luik@khk.ee>
2019-11-25 12:45:02 +0200

    prototype pärilus

--- app.js
+++ app.js
@@ -1,42 +1,31 @@
 // inimese kirjeldus OOP abil
-// inimese konstruktor
+// prototüüpide pärilus
+// isiku konstruktor
 
-function Isik(e, p, skp){
+function Isik(e, p){
     this.eesnimi = e;
     this.perenimi = p;
-    this.synnikuupaev = new Date(skp);
-
-    // väljasta täisnimi  / prototype puhul kustuta
-    this.taisNimi =function(){
-        return `${this.eesnimi} ${this.perenimi}`
-    }
-    // arvuta vanus  / prototype puhul kustuta
-    this.arvutaVanus = function(){
-        const vaheSekundites = Date.now() - this.synnikuupaev.getTime();
-        const vanusDateKujul = new Date(vaheSekundites);
-        const taisAasta = vanusDateKujul.getUTCFullYear();
-        const vanus = taisAasta - 1970;
-        return vanus;
-    }
 }
-// objekt.prototype 
-/* Isik.prototype.taisNimi = function(){
+
+Isik.prototype.taisNimi = function(){
     return `${this.eesnimi} ${this.perenimi}`;
 }
 
-Isik.prototype.arvutaVanus = function(){
-    const vaheSekundites = Date.now() - this.synnikuupaev.getTime();
-    const vanusDateKujul = new Date(vaheSekundites);
-    const taisAasta = vanusDateKujul.getUTCFullYear();
-    const vanus = taisAasta - 1970;
-    return vanus;
-} */
-Isik.prototype.abiellus = function(uusPereNimi){
-    this.perenimi = uusPereNimi;
+// kliendi konstruktor
+function Klient(e, p, t, s){
+    Isik.call(this, e, p); // konstruktori kasutamine
+    this.telefon = t;
+    this.staatus = s;
 }
-const anna = new Isik('Anna', 'Manna', '05-05-1995');
-const kadi = new Isik('Kadi', 'Padi', '03-03-1993');
-console.log(anna.taisNimi());
 
-kadi.abiellus('Vaher');
-console.log(kadi.taisNimi());
+// teiste meetodite/funktsioonide kasutamine
+Klient.prototype = Object.create(Isik.prototype);
+Klient.prototype.constructor = Klient;
+
+// määrame taisnimi funktsioon Klient objektile
+Klient.prototype.taisNimi = function(){
+    return `${this.eesnimi} ${this.perenimi} - sinul on ${this.staatus} tase`;
+}
+
+const kadi = new Klient('Kadi', 'Padi', '1234 1234', 'kuld');
+console.log(kadi.taisNimi())
